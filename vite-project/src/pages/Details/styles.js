0370e4eddbce8c899de05bbd447a4a817e542c38
almeida0808 <almeidaff08@mail.com>
2024-05-06 17:04:07 -0300

    🎉 Iniciando

---
+++ vite-project/src/pages/Details/styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.body`
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  background: red;
+  width: 100vh;
+  height: 100vw;
+`;
